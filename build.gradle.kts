3f736d4d42b1267f314c33f6ca1d0bf3e73c92c2
MarcinLoj <m.lojewski1998@gmail.com>
2023-09-11 21:47:12 +0200

    move config from code to file, add basic config

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.example"
-version = "0.0.1"
+version = "0.1.0"
 
 application {
     mainClass.set("com.example.ApplicationKt")
--- src/main/kotlin/com/example/plugins/Databases.kt
+++ src/main/kotlin/com/example/plugins/Databases.kt
@@ -9,7 +9,7 @@ import java.sql.*
 import kotlinx.coroutines.*
 
 fun Application.configureDatabases() {
-    val dbConnection: Connection = connectToPostgres(embedded = true)
+    val dbConnection: Connection = connectToPostgres(embedded = false)
     val cityService = CityService(dbConnection)
     routing {
         // Create city
@@ -70,9 +70,10 @@ fun Application.connectToPostgres(embedded: Boolean): Connection {
     if (embedded) {
         return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
-        val url = environment.config.property("postgres.url").getString()
-        val user = environment.config.property("postgres.user").getString()
-        val password = environment.config.property("postgres.password").getString()
+        //environment.config.property("postgres.url").getString()
+        val url =  "jdbc:postgresql://db:5432/msger?user=postgres"
+        val user = "postgres"
+        val password = "example"
 
         return DriverManager.getConnection(url, user, password)
     }
