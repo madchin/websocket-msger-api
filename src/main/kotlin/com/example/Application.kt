767ddcac3936c372d8dbe5a246430c5f0ad4120d
MarcinLoj <m.lojewski1998@gmail.com>
2023-09-12 18:37:56 +0200

    add ssl cert generator

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,9 +1,21 @@
 package com.example
 
 import com.example.plugins.*
+import io.ktor.network.tls.certificates.*
 import io.ktor.server.application.*
+import java.io.File
 
-fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val keyStoreFile = File("./keystore.jks")
+    val keyStore = buildKeyStore {
+        certificate("sampleAlias") {
+            password = "foobar"
+            domains = listOf("127.0.0.1", "0.0.0.0", "localhost")
+        }
+    }
+    keyStore.saveToFile(keyStoreFile, "foobar")
+    io.ktor.server.netty.EngineMain.main(args)
+}
 
 fun Application.module() {
     configureSockets()
