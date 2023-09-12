e02a0f6b07287e9c542b920a5f2d3a626ab92194
MarcinLoj <m.lojewski1998@gmail.com>
2023-09-12 18:37:34 +0200

    add temporary ssl cert, move from security and database plugin hardcoded values to application.conf

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/example/plugins/Databases.kt
+++ src/main/kotlin/com/example/plugins/Databases.kt
@@ -9,7 +9,7 @@ import java.sql.*
 import kotlinx.coroutines.*
 
 fun Application.configureDatabases() {
-    val dbConnection: Connection = connectToPostgres(embedded = false)
+    val dbConnection: Connection = connectToDatabase(embedded = false)
     val cityService = CityService(dbConnection)
     routing {
         // Create city
@@ -45,19 +45,7 @@ fun Application.configureDatabases() {
 }
 
 /**
- * Makes a connection to a Postgres database.
- *
- * In order to connect to your running Postgres process,
- * please specify the following parameters in your configuration file:
- * - postgres.url -- Url of your running database process.
- * - postgres.user -- Username for database connection
- * - postgres.password -- Password for database connection
- *
- * If you don't have a database process running yet, you may need to [download]((https://www.postgresql.org/download/))
- * and install Postgres and follow the instructions [here](https://postgresapp.com/).
- * Then, you would be able to edit your url,  which is usually "jdbc:postgresql://host:port/database", as well as
- * user and password values.
- *
+ * Makes a connection to a database.
  *
  * @param embedded -- if [true] defaults to an embedded database for tests that runs locally in the same process.
  * In this case you don't have to provide any parameters in configuration file, and you don't have to run a process.
@@ -65,15 +53,16 @@ fun Application.configureDatabases() {
  * @return [Connection] that represent connection to the database. Please, don't forget to close this connection when
  * your application shuts down by calling [Connection.close]
  * */
-fun Application.connectToPostgres(embedded: Boolean): Connection {
-    Class.forName("org.postgresql.Driver")
+fun Application.connectToDatabase(embedded: Boolean): Connection {
+    val driverClassName = environment.config.property("database.driverClassName").getString()
+    Class.forName(driverClassName)
     if (embedded) {
         return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
-        //environment.config.property("postgres.url").getString()
-        val url =  "jdbc:postgresql://db:5432/msger?user=postgres"
-        val user = "postgres"
-        val password = "example"
+
+        val url = environment.config.property("database.jdbcUrl").getString()
+        val user = environment.config.property("database.user").getString()
+        val password = environment.config.property("database.password").getString()
 
         return DriverManager.getConnection(url, user, password)
     }
