29222d8f0be76fc06f72b659fcdd96235b050bef
MarcinLoj <m.lojewski1998@gmail.com>
2023-10-16 20:48:54 +0200

    remove Result<T> handlers from controller layer and responses which based on them

--- src/main/kotlin/com/example/controller/feature_sign_in_up/SignInUpRoutes.kt
+++ src/main/kotlin/com/example/controller/feature_sign_in_up/SignInUpRoutes.kt
@@ -2,12 +2,9 @@ package com.example.controller.feature_sign_in_up
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.example.controller.util.ErrorResponse
-import com.example.controller.util.ErrorType
-import com.example.domain.model.User
-import com.example.domain.dao.service.UserService
-import com.example.data.util.GenericException
 import com.example.data.util.UserSession
+import com.example.domain.dao.service.UserService
+import com.example.domain.model.User
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -26,72 +23,40 @@ fun Route.signInUp(userService: UserService) {
     post("/sign-in") {
         val receivedUser = call.receive<User>()
 
-        userService.getUser(receivedUser).also { result ->
-            result.onSuccess {
-                val sevenDaysInMs = 60000L * 60000 * 24 * 7
-                val token = JWT.create()
-                    .withAudience(jwtAudience)
-                    .withIssuer(jwtDomain)
-                    .withClaim("username", it.username)
-                    .withExpiresAt(Date(System.currentTimeMillis() + sevenDaysInMs))
-                    .sign(Algorithm.HMAC256(jwtSecret))
+        userService.getUser(receivedUser).also { user ->
+            val sevenDaysInMs = 60000L * 60000 * 24 * 7
+            val token = JWT.create()
+                .withAudience(jwtAudience)
+                .withIssuer(jwtDomain)
+                .withClaim("username", user.username)
+                .withExpiresAt(Date(System.currentTimeMillis() + sevenDaysInMs))
+                .sign(Algorithm.HMAC256(jwtSecret))
 
-                call.sessions.set(UserSession(uid = it.id ?: ""))
-                call.respond(hashMapOf("token" to token, "uid" to it.id))
-            }
-            result.onFailure {
-                val message = result.exceptionOrNull()?.message ?: ""
-                val type =
-                    if (message != GenericException().message) ErrorType.NOT_FOUND.name else ErrorType.GENERIC.name
-                val statusCode =
-                    if (type == ErrorType.NOT_FOUND.name) HttpStatusCode.NotFound else HttpStatusCode.BadRequest
-                call.respond(
-                    statusCode,
-                    ErrorResponse(type = type, message = message)
-                )
-            }
+            call.sessions.set(UserSession(uid = user.id ?: ""))
+            call.respond(hashMapOf("token" to token, "uid" to user.id))
+
         }
     }
 
 
-authenticate("auth-oauth-google") {
-    get("login") {
-        call.respondRedirect("/callback")
-    }
+    authenticate("auth-oauth-google") {
+        get("login") {
+            call.respondRedirect("/callback")
+        }
 
-    get("/callback") {
-        val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-        //call.sessions.set(UserSession(principal?.accessToken.toString()))
-        call.respondRedirect("/hello")
-    }
-}
-
-post("/sign-up") {
-    val receivedUser = call.receive<User>()
-
-    receivedUser.let { user ->
-
-        userService.createUser(User(username = user.username, password = user.password, email = "temp")).also { result ->
-            result.onSuccess {
-                call.respond(HttpStatusCode.Created)
-            }
-
-            result.onFailure {
-                val message = result.exceptionOrNull()?.message ?: ""
-                val type = when (message) {
-                    GenericException().message -> ErrorType.GENERIC.name
-                    "User with ${user.username} already exists" -> ErrorType.ALREADY_EXISTS.name
-                    else -> ErrorType.NOT_FOUND.name
-                }
-                val statusCode =
-                    if (type == ErrorType.NOT_FOUND.name) HttpStatusCode.NotFound else HttpStatusCode.BadRequest
-                call.respond(
-                    statusCode,
-                    ErrorResponse(type = type, message = message)
-                )
-            }
+        get("/callback") {
+            val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
+            //call.sessions.set(UserSession(principal?.accessToken.toString()))
+            call.respondRedirect("/hello")
         }
     }
-}
+
+    post("/sign-up") {
+        val user = call.receive<User>()
+        userService.createUser(User(username = user.username, password = user.password, email = "temp")).also {
+            call.respond(HttpStatusCode.Created)
+        }
+
+    }
 
 }
