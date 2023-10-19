099add96437c971ee063a276aab4e1db34e93914
MarcinLoj <m.lojewski1998@gmail.com>
2023-10-19 22:49:43 +0200

    change check password method

--- src/main/kotlin/com/example/util/PasswordHasher.kt
+++ src/main/kotlin/com/example/util/PasswordHasher.kt
@@ -3,7 +3,12 @@ package com.example.util
 import org.mindrot.jbcrypt.BCrypt
 
 object PasswordHasher {
-    fun checkPassword(attempt:String, hashed: String) = BCrypt.checkpw(attempt, hashed)
+    fun checkPassword(attempt: String, hashed: String) {
+        val isPasswordCorrect = BCrypt.checkpw(attempt, hashed)
+        if (!isPasswordCorrect) {
+            throw ExplicitException.WrongCredentials
+        }
+    }
 
     fun hashPassword(password: String) = BCrypt.hashpw(password, BCrypt.gensalt())
 }
